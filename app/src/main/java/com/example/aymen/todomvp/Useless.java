397020909e335b250feb041680e1e015327831c7
aymenoppa <aymenlabidi21@gmail.com>
2017-08-08 13:21:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/aymen/todomvp/Useless.java
@@ -0,0 +1,13 @@
+package com.example.aymen.todomvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Useless extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_useless);
+    }
+}
